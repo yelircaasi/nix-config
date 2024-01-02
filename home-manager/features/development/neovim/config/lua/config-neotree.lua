46a89928b87a4d9e32c0c0aec1af2554ccc10147
yelircaasi <isaac.r.riley@gmail.com>
2024-01-02 09:40:41 +0100

    feat: move icons to separate folder in neovim config

--- home-manager/features/development/neovim/config/lua/config-neotree.lua
+++ home-manager/features/development/neovim/config/lua/config-neotree.lua
@@ -35,7 +35,7 @@ neotree.setup({
       icon = {
         folder_closed = "",
         folder_open = "",
-        folder_empty = "ﰊ",
+        folder_empty = "",
         -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
         -- then these will never be used.
         default = "*",
@@ -154,8 +154,8 @@ neotree.setup({
           --".null-ls_*",
         },
       },
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
-                                  -- time the current file is changed while the tree is open.
+      follow_current_file = {enabled = true}, -- This will find and focus the file in the active buffer every
+                                              -- time the current file is changed while the tree is open.
       group_empty_dirs = false, -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
                                               -- in whatever position is specified in window.position
@@ -189,8 +189,8 @@ neotree.setup({
       commands = {} -- Add a custom command or override a global one using the same function name
     },
     buffers = {
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
-                                   -- time the current file is changed while the tree is open.
+      follow_current_file = {enabled = true}, -- This will find and focus the file in the active buffer every
+                                              -- time the current file is changed while the tree is open.
       group_empty_dirs = true, -- when true, empty folders will be grouped together
       show_unloaded = true,
       window = {

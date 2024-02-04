aaf6baf73022ffd316b2bab2a2bc34ee6f9008bb
yelircaasi <isaac.r.riley@gmail.com>
2024-02-04 11:46:36 +0100

    feat: pin all inputs to a specific revision -> manual updates in exchange for total solidity

--- flake.nix
+++ flake.nix
@@ -3,17 +3,28 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/f4a8d6d5324c327dcc2d863eb7f3cc06ad630df4";
+      #url = "github:nixos/nixpkgs/nixos-23.11";
+    };
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/10cd9c53115061aa6a0a90aad0b0dde6a999cdb9";
+      #url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Neovim
     neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
+      url = "github:nix-community/neovim-nightly-overlay/1da2e054a16309d7d7f7669438c8b9a5ef1b4642";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # SwayFX
+
+    swayfx = {
+      url = "github:WillPower3309/swayfx/f0ecffe593574593ec99c104fa12e4bfd593b0f2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

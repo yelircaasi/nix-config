96f150daefc54175f75c46f692322fd239f82f30
yelircaasi <isaac.r.riley@gmail.com>
2024-03-04 06:22:29 +0100

    fix: revert to gdm to leep things working for now

--- system/modules/display-manager.nix
+++ system/modules/display-manager.nix
@@ -4,9 +4,9 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    sddm
+    # sddm
     # where-is-my-sddm-theme
-    sddm-chili-theme
+    # sddm-chili-theme
 
     # lightdm
     # lightdm-gtk-greeter
@@ -15,14 +15,48 @@
   # services.xserver.displayManager.lightdm.greeter.package = pkgs.lightdm-gtk-greeter;
   # services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
 
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.displayManager.sddm.theme = "chili";
+  # services.xserver.displayManager.sddm = {
+  #   enable = true;
+  #   theme = "chili";
+  #   enableHidpi = true;
+  #   wayland.enable = true;
+  #   autoLogin = {
+  #     relogin = true;
+  #   };
+  #   stopScript = ''
+  #     # pkill -15 swww-daemon
+  #     # if [ -f /run/user/1000/swww.socket ]
+  #     # then
+  #     #   rm /run/user/1000/swww.socket
+  #     # fi
+  #     pkill -9 Hyprland
+  #   '';
+  #   # services.xserver.desktopManager.runXdgAutostartIfNone = true;
+  # };
+  #wservices.xserver.displayManager.sddm.theme = "chili";
   # services.xserver.displayManager.sddm.enableHidpi = true;
   # services.xserver.displayManager.sddm.wayland.enable = true;
-  # services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    # wayland = true;
+    # autoLogin.delay = 10;
+  };
   # services.xserver.desktopManager.gnome.enable = false;
 
+  services.xserver.displayManager.defaultSession = "hyprland";
   services.xserver.displayManager.hiddenUsers = ["landscape"];
+  # services.xserver.displayManager.autoLogin.enable = true;
+  # services.xserver.displayManager.autoLogin.user = "isaac";
+
+  # systemd.extraConfig = ''
+  #   DefaultTimeoutStopSec=10s
+  # '';
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-wlr];
+    # config.common.default = "*";
+  };
 }

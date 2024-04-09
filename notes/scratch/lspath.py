1f7c26acd9f2a267c6c642b26c4193074b2d6bc2
yelircaasi <isaac.r.riley@gmail.com>
2024-04-09 18:34:17 +0200

    chore: backup notes

---
+++ notes/scratch/lspath.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+paths = os.environ["PATH"].split(":")
+for p in paths:
+    print(p)
+    print()
+    print("\n".join(sorted(os.listdir(p))))
+    print("\n")
+

e5b97c6f9be777a7608a9ec59efbd3be9c41c9bf
yelircaasi <yelircaasi@proton.me>
2026-02-06 14:29:45 +0100

    add new plugins lookup lua file

--- home/applications/console/neovim/default.nix
+++ home/applications/console/neovim/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
-let pde = pkgs.symlinkJoin {
+let
+pde = pkgs.symlinkJoin {
   name = "pde";
   paths = [ pkgs.neovim ];
   buildInputs = [ pkgs.makeWrapper ];
@@ -12,18 +13,18 @@ let pde = pkgs.symlinkJoin {
         pkgs.rust-analyzer
         pkgs.rustfmt
         pkgs.clippy
-        
+
         # Haskell tooling
         pkgs.haskell-language-server
         pkgs.fourmolu
         pkgs.hlint
-        
+
         # Python tooling
         pkgs.pyright
         pkgs.ruff
         pkgs.black
         pkgs.isort
-        
+
         # General utilities
         pkgs.tree-sitter
         pkgs.fd
@@ -42,7 +43,35 @@ let pde = pkgs.symlinkJoin {
     license = licenses.asl20;
     platforms = platforms.unix;
   };
-}; in
+};
+
+myTreesitter = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+  myPlugins = with pkgs.vimPlugins; [
+    oil-nvim
+    blink-cmp
+    luasnip
+    rustaceanvim
+    myTreesitter
+  ];
+
+  idLookup = {}; # for plugins lacking p.src.owner and p.src.repo
+
+  toLuaTable = plugins: 
+    let
+      getPluginId = p: 
+        if (p ? src && p.src ? owner && p.src ? repo) then
+          "${p.src.owner}/${p.src.repo}"
+        else
+          idLookup.${p.pname};
+
+      lines = map (p: ''  ["${getPluginId p}"] = { path = "${p}" },'') plugins;
+    in
+    "return {\n" + (builtins.concatStringsSep "\n" lines) + "\n}\n";
+
+
+
+in
 {
-  home.packages = [pde];
+  home.packages = [pde ];
+  home.file.".config/nvim/lua/nix_plugins.lua".text = toLuaTable myPlugins;
 }

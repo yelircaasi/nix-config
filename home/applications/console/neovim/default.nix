14b7a39a4e4b6796e6c4e02c37b68e16231e084b
yelircaasi <yelircaasi@proton.me>
2026-02-07 14:01:52 +0100

    add remaining plugins from previous config

--- home/applications/console/neovim/default.nix
+++ home/applications/console/neovim/default.nix
@@ -1,4 +1,8 @@
-{inputs, pkgs, ...}: let
+{
+  inputs,
+  pkgs,
+  ...
+}: let
   neovim-nightly = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
 
   pde = pkgs.symlinkJoin {
@@ -46,14 +50,87 @@
   };
 
   myTreesitter = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
-  myPlugins = with pkgs.vimPlugins; [
+
+  myPlugins = with pkgs.vimPlugins; let
+    markit-nvim = pkgs.vimUtils.buildVimPlugin {
+      pname = "markit.nvim";
+      version = "2025-10-09";
+      src = pkgs.fetchFromGitHub {
+        owner = "2KAbhishek";
+        repo = "markit.nvim";
+        rev = "c716195d5b0b21ef03a20a1facc46d33ca9f7c49";
+        sha256 = "sha256-FxdbKmxrhwXWBzjPve07oqBCv3lp++YwZJPuLfaFOIU=";
+      };
+      meta.homepage = "https://github.com/2KAbhishek/markit.nvim";
+    };
+    pickme-nvim = pkgs.vimUtils.buildVimPlugin {
+      pname = "pickme.nvim";
+      version = "2025-12-24";
+      src = pkgs.fetchFromGitHub {
+        owner = "2KAbhishek";
+        repo = "pickme.nvim";
+        rev = "3bfd63fa0a1fa362afc9dfa86b83100e75903e6b";
+        sha256 = "sha256-FxdbKmxrhwXWBzjPve07oqBCv3lp++YwZJPuLfaFOIU=";
+      };
+      meta.homepage = "https://github.com/2KAbhishek/pickme.nvim";
+    };
+  in [
     oil-nvim
     blink-cmp
+    mini-pick
     luasnip
     rustaceanvim
     myTreesitter
-  ];
+    haskell-tools-nvim
+    # vague-nvim
+    bamboo-nvim
 
+    zen-mode-nvim
+    lualine-nvim
+    dial-nvim
+    yazi-nvim
+    conform-nvim
+    toggleterm-nvim
+    vim-floaterm
+    #floaterm
+    which-key-nvim
+    nvim-bqf
+    mini-nvim
+    gitsigns-nvim
+    todo-comments-nvim
+    telescope-nvim
+    plenary-nvim
+    diffview-nvim
+    markit-nvim
+    pickme-nvim
+    nvim-treesitter-textobjects
+    neotest
+    vim-visual-multi
+
+    # nvim-lualine/lualine.nvim,
+    # monaqa/dial.nvim,
+    # mikavilpas/yazi.nvim,
+    # stevearc/conform.nvim,
+    # akinsho/toggleterm.nvim,
+    # voldikss/vim-floaterm,
+    # folke/which-key.nvim,
+    # kevinhwang91/nvim-bqf,
+    # echasnovski/mini.nvim,
+    # lewis6991/gitsigns.nvim,
+    # folke/todo-comments.nvim,
+    # nvim-telescope/telescope.nvim,
+    # nvim-lua/plenary.nvim, -- dependency of telescope
+    # sindrets/diffview.nvim,
+    # 2kabhishek/markit.nvim,
+    # 2KAbhishek/pickme.nvim,
+    # nvim-treesitter/nvim-treesitter-textobjects,
+    # nvim-neotest/neotest,
+    # mg979/vim-visual-multi
+  ];
+  #  ['oil.nvim'] = { src = gh("stevearc/oil.nvim") },
+  #  ['blink.cmp'] = { src = gh("saghen/blink.cmp") },
+  #  ["mini.pick"] = { src = gh("nvim-mini/mini.pick") },
+  #  ["nvim-treesitter"] = { src = gh("nvim-treesitter/nvim-treesitter") },
   idLookup = {}; # for plugins lacking p.src.owner and p.src.repo
 
   ghUrl = "https://github.com/";

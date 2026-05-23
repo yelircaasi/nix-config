24128c5e416cc752c3af4d44a437515e0903ce13
yelircaasi <yelircaasi@proton.me>
2026-05-23 23:47:21 +0200

    change wezterm colors to odenwald

--- home/applications/gui/terminal-emulator/wezterm/default.nix
+++ home/applications/gui/terminal-emulator/wezterm/default.nix
@@ -13,4 +13,5 @@
   # programs.wezterm.enableZshIntegration= true ;
 
   xdg.configFile."./wezterm/wezterm.lua".text = g.utils.readAndInterpolate g ./wezterm.lua;
+  xdg.configFile."./wezterm/colors/odenwald.toml".text = builtins.readFile ./odenwald.toml;
 }
